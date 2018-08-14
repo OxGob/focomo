343cc55c1d729c637cc217374615be97ebfaaded
oxgob <arvingo@yahoo.co.uk>
2018-08-14 14:45:02 +0100

    Added a page for later

--- foc/src/pages/Index.vue
+++ foc/src/pages/Index.vue
@@ -1,5 +1,13 @@
 <template>
   <q-page class="bg-cyan-2 q-ma-xl">
+    <!-- BEGIN Section for trying out tabs and pages -->
+    <div>
+      <q-tabs align="justify">
+        <q-route-tab slot="title" name="tab-1" icon="subject" label="Main P" to="/" exact />
+        <q-route-tab slot="title" name="tab-2" icon="fingerprint" label="Test P1" to="/testP1" exact />
+      </q-tabs>
+    </div>
+    <!-- CLOSE Section for trying out tabs and pages -->
     <!-- Added button to open modal -->
     <q-btn label="Open Modal" color="purple" @click="openModal" />
      <q-modal v-model="opened">
@@ -23,7 +31,8 @@ export default {
   },
   data () {
     return {
-      opened: true
+      opened: true,
+      mainObj: ''
     }
   },
   methods: {
---
+++ foc/src/pages/testPg1.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page padding>
+    <div>
+      <q-tabs align="justify">
+        <q-route-tab slot="title" name="tab-1" icon="subject" label="Main P" to="/" exact />
+        <q-route-tab slot="title" name="tab-2" icon="fingerprint" label="Test P1" to="/testP1" exact />
+      </q-tabs>
+    </div>
+    <h6> This is testPg1</h6>
+    <q-field label="Title Name: ">
+      <q-input
+      v-model= "nameTitle"
+       placeholder="Enter the name" />
+    </q-field>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      nameTitle: ''
+    }
+  }
+}
+</script>
